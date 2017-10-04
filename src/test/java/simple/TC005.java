10ea7780a2b77ec3b7eb60d732b7af7cba5de966
BrianTessele <briantessele@gmail.com>
2017-10-04 19:14:47 -0300

    Test Case 005 was automated

---
+++ src/test/java/simple/TC005.java
@@ -0,0 +1,54 @@
+package simple;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+import org.hamcrest.Matchers;
+//import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC005 {
+
+	@Test
+	public void test() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/PC 09/Desktop/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.navigate().to("http://www.buttonline.com.br/data/mx_login.php");
+		// String title = driver.getTitle();
+		// Thread.sleep(500);
+		// driver.quit();
+
+		// Preenche o Email com o valor "brian.roller@hotmail.com"
+		WebElement inputEmail = driver.findElement(By.name("email"));
+		inputEmail.sendKeys("brian.roller@hotmail.com");
+		// Preenche a Senha com o valor "123"
+		WebElement inputSenha = driver.findElement(By.name("senha"));
+		inputSenha.sendKeys("123");
+
+		// Clica no botão Login
+		WebElement buttonLogin = driver.findElement(By.cssSelector("input[type='submit'"));
+		buttonLogin.click();
+		
+		//Clica no banner "Experimente o Sistema"
+		driver.findElement(By.cssSelector("a[href*='buttonlinebeta']")).click();
+
+
+		// Verifica se existe o botão Help.
+		WebElement helpButton = driver.findElement(By.xpath("//a[@class='btn btn-help navbar-btn ng-binding']"));
+		assertNotNull(helpButton);
+		helpButton.click();
+		
+		// Por meio da asserção Hamcrest, procura pela string “Instruções"
+		String codigoPagina = driver.getPageSource();
+		assertThat(codigoPagina, Matchers.containsString("Instruções"));
+		
+		
+	}
+
+}
