608b45ef38538b997d387ac70101bedbde76a20a
Thiago Toazza <thiagotoazza@outlook.com>
2017-10-11 14:11:42 -0300

    #27

---
+++ src/test/java/simple/TC002.java
@@ -0,0 +1,61 @@
+package simple;
+
+//import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+//import org.hamcrest.MatcherAssert;
+//import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC002 {
+
+	@Test
+	public void test() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/teste/Desktop/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.navigate().to("http://buttonline.com.br/data/mx_cadastro.php");
+
+		// Fill the field with "fulano@gmail,com"
+		WebElement inputEmail = driver.findElement(By.name("email"));
+		inputEmail.sendKeys("fulano@gmail,com");
+        Thread.sleep(2000); 
+        
+        // Fill the password field with "123"
+		WebElement inputSenha = driver.findElement(By.name("senha"));
+		inputSenha.sendKeys("123");
+        Thread.sleep(2000);
+        
+        // Fill confirm password with the value of "123"
+		WebElement inputSenhaConf = driver.findElement(By.name("senha_conf"));
+		inputSenhaConf.sendKeys("123");
+        Thread.sleep(2000);
+        
+        // Complete the field name with "Fulana"
+		WebElement inputNome = driver.findElement(By.name("nome"));
+		inputNome.sendKeys("Fulana");
+		Thread.sleep(2000);
+		
+		// Complete the field of occupation with the value of "Tester"
+		WebElement inputProfissao = driver.findElement(By.name("profissao"));
+		inputProfissao.sendKeys("Tester");
+		Thread.sleep(2500);
+
+		// Click in 'Enviar' button
+		WebElement buttonEnviar = driver.findElement(By.cssSelector("input[value='Enviar'"));
+		buttonEnviar.click();
+		Thread.sleep(1000);
+		
+		// Verify if the system returns the error message.
+		String expected = "Por favor, preencha corretamente o campo 'E-mail'.";
+		String actual = driver.switchTo().alert().getText();
+					
+			assertEquals(expected, actual);
+			
+		driver.quit();
+	}
+}
---
+++ src/test/java/simple/TC006.java
@@ -0,0 +1,41 @@
+package simple;
+
+//import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+//import org.hamcrest.MatcherAssert;
+//import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC006 {
+
+	@Test
+	public void test() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/teste/Desktop/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.navigate().to("http://buttonline.com.br/data/mx_cadastro.php");
+
+		// Fill the e-mail field with "brian.roller@hotmail.com"
+		WebElement inputEmail = driver.findElement(By.name("email"));
+		inputEmail.sendKeys("brian.roller@hotmail.com");
+        Thread.sleep(2000); 
+        
+        // Fill the password field with "123"
+		WebElement inputSenha = driver.findElement(By.name("senha"));
+		inputSenha.sendKeys("123");
+		Thread.sleep(1000);
+       
+		// Verify if the system returns the error message.
+		String expected = "E-mail ja cadastrado !";
+		String actual = driver.switchTo().alert().getText();
+					
+			assertEquals(expected, actual);
+		
+		driver.quit();
+	}
+}
