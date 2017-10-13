c560a58fee3dae5c5a534cda75d675a140f12e70
Brian Tessele <briantessele@gmail.com>
2017-10-12 22:41:17 -0300

    Changed code

--- src/test/java/funcionalTest/CadastroValidoComPageObjectTest.java
+++ src/test/java/funcionalTest/CadastroValidoComPageObjectTest.java
@@ -14,22 +14,24 @@ import pageObjects.RegisterPage;
 
 public class CadastroValidoComPageObjectTest {
 
-	private WebDriver driver;
+	//private WebDriver driver;
 
-	@Before
+	/*@Before
 	public void before() {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Brian Tessele/Desktop/chromedriver.exe");
-		driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-
+	} */
+	
 	@Test
 	public void cadastroValido() throws InterruptedException {
-
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Brian Tessele/Desktop/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
 		RegisterPage registerPage = new RegisterPage(driver);
 		HomePage homePage = new HomePage(driver);
 			registerPage.visita("http://buttonline.com.br/data/mx_cadastro.php").
-			autentica("Nasdasovso@gmail.com", "123", "123","Brian", "tester");
+			autentica("aa276555@gmail.com", "123", "123","Brian", "tester");
 			
 		
 		assertTrue(homePage.cadastroEfetuadoComSucesso());
@@ -39,9 +41,9 @@ public class CadastroValidoComPageObjectTest {
 		assertTrue(homePage.isValida());
 	}
 
-	@After
-	public void after() {
-		driver.quit();
-	}
+	//@After
+	//public void after() {
+		//driver.quit();
+	//}
 
 }
